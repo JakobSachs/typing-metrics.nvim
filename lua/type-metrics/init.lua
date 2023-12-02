e1f7a14e45456d593db5f48358182e6137a7cda4
Jakob Sachs <jakobsachs1999@gmail.com>
2023-12-02 19:19:35 +0100

    Got first ssomewhat working prototype

--- lua/type-metrics/init.lua
+++ lua/type-metrics/init.lua
@@ -3,43 +3,77 @@ local M = {}
 
 M.start_time = nil
 M.char_count = 0
-M.max = 0
+M.history = {}
 
 -- Function to get current time
 local function get_current_time()
-    return vim.loop.hrtime() / 1e9 -- high-resolution time in seconds
+	return vim.loop.hrtime() / 1e9 -- high-resolution time in seconds
 end
 
--- Function to calculate and display WPM
+-- Function to calculate wpm and add it to history
 local function calculate_wpm()
-    local current_time = get_current_time()
-    if M.start_time == nil then
-        M.start_time = current_time
-    end
-    local elapsed_time = current_time - M.start_time
+	if M.start_time == nil then
+		return
+	end
+	local elapsed_time = get_current_time() - M.start_time
+	local wpm = math.floor(M.char_count / 5 / (elapsed_time / 60))
 
-    if elapsed_time > 0 then
-        local wpm = (M.char_count / 5) / (elapsed_time / 60)
-        M.max = math.max(M.max, wpm)
-        print(string.format("Current WPM: %.2f\n", wpm))
-    end
+	table.insert(M.history, wpm)
+	if #M.history > 10 then
+		table.remove(M.history, 1)
+	end
 end
 
 -- Function to update character count and calculate WPM
 function M.on_text_changed()
-    M.char_count = M.char_count + 1
-    calculate_wpm()
+	M.char_count = M.char_count + 1
+	calculate_wpm()
+end
+
+function M.insert_start()
+	M.start_time = get_current_time()
+	M.char_count = 0
+end
+
+function M.insert_stop()
+	calculate_wpm()
+	M.start_time = nil
+	M.char_count = 0
 end
 
 function M.setup()
-    -- Setup Autocmds for TextChanged and TextChangedI
-    vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-        callback = M.on_text_changed,
-    })
+	-- Setup Autocmds for TextChanged and TextChangedI
+	vim.api.nvim_create_autocmd({ "InsertCharPre" }, {
+		callback = M.on_text_changed,
+	})
+
+	vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+		callback = M.insert_start,
+	})
+
+	vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+		callback = M.insert_stop,
+	})
 end
 
-function M.get_max()
-    return M.max
+-- API functions for external use
+
+-- Function to get average WPM from history
+function M.get_avg()
+	local sum = 0
+	for _, v in ipairs(M.history) do
+		sum = sum + v
+	end
+	return sum / #M.history
+end
+
+-- Function to get history
+function M.get_history()
+	return M.history
+end
+
+function M.get_statusline()
+	return string.format("WPM: %d", M.get_avg())
 end
 
 return M
--- plugin/type-metrics.lua
+++ plugin/type-metrics.lua
@@ -1,9 +1,13 @@
 -- Setup functions etc for this plugin module
-local init = require("type-metrics.init")
-init.setup()
+local typ = require("type-metrics.init")
+typ.setup()
 
-vim.api.nvim_create_user_command("TypeMetricsMax",
-    function()
-        print(init.get_max(), "max")
-    end, {}
-)
+vim.api.nvim_create_user_command("TypeMetricsAvg", function()
+    print(typ.get_avg())
+end, {})
+
+vim.api.nvim_create_user_command("TypeMetricsHistory", function()
+    print(vim.inspect(typ.history))
+end, {})
+
+return typ
