b03e754d1fad62b809836d3bfa345d512a3ff997
Jakob Sachs <jakobsachs1999@gmail.com>
2023-12-02 18:36:14 +0100

    Changed commadn name

--- plugin/type-metrics.lua
+++ plugin/type-metrics.lua
@@ -2,7 +2,7 @@
 local init = require("type-metrics.init")
 init.setup()
 
-vim.api.nvim_create_user_command("TypeMetrics max",
+vim.api.nvim_create_user_command("TypeMetricsMax",
     function()
         print(init.get_max(), "max")
     end, {}
