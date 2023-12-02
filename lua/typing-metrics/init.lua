fc2617c42b81f1da3df248fa666650628dacea69
Jakob Sachs <jakobsachs1999@gmail.com>
2023-12-02 20:56:59 +0100

    Added timer based updates to make UI more responsive

--- lua/typing-metrics/init.lua
+++ lua/typing-metrics/init.lua
@@ -2,104 +2,64 @@
 local M = {}
 
 M.config = {
-	-- Default config values
-	word_length = 5,
-	update_interval = 1000,
-	history_size = 10,
-	target_wpm = 60,
-	bar_direction = "vertical",
+    -- Default config values
+    word_length = 5,
+    update_interval = 500,
+    average_size = 3,
+    target_wpm = 60,
+    bar_direction = "vertical",
 }
 
-M.start_time = nil
 M.char_count = 0
-M.history = {}
+M.average = {}
 
-function M.setup(user_config)
-	if user_config == nil then
-		user_config = {}
-	end
 
-	-- Merge user config with default config
-	for k, v in pairs(user_config) do
-		M.config[k] = v
-	end
-
-	-- Setup Autocmds for TextChanged and TextChangedI
-	vim.api.nvim_create_autocmd({ "InsertCharPre" }, {
-		callback = M.on_text_changed,
-	})
-
-	vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-		callback = M.insert_start,
-	})
-
-	vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-		callback = M.insert_stop,
-	})
-end
 
 -- Function to get current time
 local function get_current_time()
-	return vim.loop.hrtime() / 1e9 -- high-resolution time in seconds
+    return vim.loop.hrtime() / 1e9 -- high-resolution time in seconds
 end
 
--- Function to calculate wpm and add it to history
 local function update_wpm()
-	if M.start_time == nil then -- If start_time is nil, we are not typing
-		return
-	end
+    local wpm = math.floor(M.char_count / M.config.word_length * 60)
+    table.insert(M.average, wpm)
 
-	local elapsed_time = get_current_time() - M.start_time
-	-- If the time elapsed is less than the update interval, do nothing
-	if elapsed_time < M.config.update_interval / 1000 then
-		return
-	end
+    if #M.average > M.config.average_size then
+        table.remove(M.average, 1)
+    end
 
-	local wpm = math.floor(M.char_count / M.config.word_length / (elapsed_time / 60))
-
-	table.insert(M.history, wpm)
-
-	if #M.history > M.config.history_size then
-		table.remove(M.history, 1)
-	end
+    M.char_count = 0
 end
+
+
 
 -- Function to update character count and calculate WPM
 function M.on_text_changed()
-	M.char_count = M.char_count + 1
-	update_wpm()
+    M.char_count = M.char_count + 1
 end
 
 function M.insert_start()
-	M.start_time = get_current_time()
-	M.char_count = 0
+    M.char_count = 0
 end
 
 function M.insert_stop()
-	update_wpm()
-	M.start_time = nil
-	M.char_count = 0
+    M.char_count = 0
 end
 
 -- API functions for external use
 
--- Function to get average WPM from history
+-- Function to get average WPM from average
 function M.get_avg()
-	local sum = 0
-	for _, v in ipairs(M.history) do
-		sum = sum + v
-	end
-	return sum / #M.history
-end
-
--- Function to get history
-function M.get_history()
-	return M.history
+    local sum = 0
+    for _, v in ipairs(M.average) do
+        sum = sum + v
+    end
+    return sum / #M.average
 end
 
 -- Function to get statusline-component as a string
 function M.get_statusline_raw()
-	return string.format("WPM: %3d", M.get_avg())
+    return string.format("WPM: %3d", M.get_avg())
 end
 
 -- Function to get statusline-component as a nice unicode bar
@@ -107,17 +67,50 @@ M.v_bar = { "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
 M.h_bar = { "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█" }
 
 function M.get_statusline()
-	local wpm = M.get_avg()
-	local bar = ""
-	local bar_idx = math.min(math.floor(wpm / M.config.target_wpm * 8), #M.v_bar - 1)
+    local wpm = M.get_avg()
+    local bar = ""
+    local bar_idx = math.min(math.floor(wpm / M.config.target_wpm * 8), #M.v_bar - 1)
 
-	local bar_char = M.v_bar
-	if M.config.bar_direction == "horizontal" then
-		bar_char = M.h_bar
-	end
+    local bar_char = M.v_bar
+    if M.config.bar_direction == "horizontal" then
+        bar_char = M.h_bar
+    end
 
-	bar = bar_char[bar_idx + 1]
-	return bar
+    bar = bar_char[bar_idx + 1]
+    return bar
+end
+
+-- Start a Timer
+function M.start_timer()
+    local timer = vim.loop.new_timer()
+    timer:start(M.config.update_interval, M.config.update_interval, vim.schedule_wrap(update_wpm)) -- 1000ms delay, 1000ms interval
+end
+
+function M.setup(user_config)
+    if user_config == nil then
+        user_config = {}
+    end
+
+    -- Merge user config with default config
+    for k, v in pairs(user_config) do
+        M.config[k] = v
+    end
+
+    -- Setup Autocmds for TextChanged and TextChangedI
+    vim.api.nvim_create_autocmd({ "InsertCharPre" }, {
+        callback = M.on_text_changed,
+    })
+
+    -- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+    --     callback = M.insert_start,
+    -- })
+
+    -- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    --     callback = M.insert_stop,
+    -- })
+
+    -- Start timer
+    M.start_timer()
 end
 
 -- return module table
--- plugin/typing-metrics.lua
+++ plugin/typing-metrics.lua
@@ -3,11 +3,11 @@ local typ = require("typing-metrics.init")
 typ.setup()
 
 vim.api.nvim_create_user_command("TypeMetricsAvg", function()
-	print(typ.get_avg())
+    print(typ.get_avg())
 end, {})
 
 vim.api.nvim_create_user_command("TypeMetricsHistory", function()
-	print(vim.inspect(typ.history))
+    print(vim.inspect(typ.history))
 end, {})
 
 return typ
