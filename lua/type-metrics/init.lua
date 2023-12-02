bb347423811ee2f32cf0488a01465f59a5a27e5b
Jakob Sachs <jakobsachs1999@gmail.com>
2023-12-02 18:14:51 +0100

    first speed test

--- lua/type-metrics/init.lua
+++ lua/type-metrics/init.lua
@@ -1,12 +1,33 @@
 -- Main file for type-metrics.nvim
 local M = {}
 
-function M.setup(opts)
-    print("TypeMetrics setup")
+local key_count = 0
+local start_time = nil
+local last_update = nil
 
-    vim.register_keystroke_callback(function(key)
-        print("Key pressed: " .. key)
-    end)
+local function get_current_time()
+    return vim.loop.hrtime() / 1e9 -- high-resolution time in seconds
 end
 
-return
+function M.on_key_pressed(key)
+    if start_time == nil then
+        start_time = get_current_time()
+    end
+
+    key_count = key_count + 1
+    last_update = get_current_time()
+
+    M.update_wpm() -- Update WPM every time a key is pressed
+end
+
+function M.update_wpm()
+    local current_time = get_current_time()
+    local elapsed_time = current_time - start_time
+
+    if elapsed_time > 0 then
+        local wpm = (key_count / 5) / (elapsed_time / 60) -- Average word length is considered 5 characters
+        print(string.format("Current WPM: %.2f", wpm))    -- Display WPM in the command line
+    end
+end
+
+return M
--- plugin/type-metrics.lua
+++ plugin/type-metrics.lua
@@ -1,1 +1,6 @@
 -- Setup functions etc for this plugin module
+local init = require("type-metrics.init")
+
+vim.register_keystroke_callback(function(key)
+    init.on_key_pressed(key)
+end, 'tracker')
